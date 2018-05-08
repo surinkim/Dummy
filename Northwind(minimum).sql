0141e0f6f55ced2b2705109190bc876cafee88f1
Hyunuk Kim <nnhope@hotmail.com>
2018-05-08 13:39:50 +0900

    Update Northwind(minimum).sql

---
+++ Northwind(minimum).sql
@@ -0,0 +1,38 @@
+/*
+** Copyright Microsoft, Inc. 1994 - 2000
+** All Rights Reserved.
+*/
+
+SET NOCOUNT ON
+GO
+
+USE master
+GO
+if exists (select * from sysdatabases where name='Northwind')
+		drop database Northwind
+go
+
+DECLARE @device_directory NVARCHAR(520)
+SELECT @device_directory = SUBSTRING(filename, 1, CHARINDEX(N'master.mdf', LOWER(filename)) - 1)
+FROM master.dbo.sysaltfiles WHERE dbid = 1 AND fileid = 1
+
+EXECUTE (N'CREATE DATABASE Northwind
+  ON PRIMARY (NAME = N''Northwind'', FILENAME = N''' + @device_directory + N'northwnd.mdf'')
+  LOG ON (NAME = N''Northwind_log'',  FILENAME = N''' + @device_directory + N'northwnd.ldf'')')
+go
+
+set quoted_identifier on
+GO
+
+/* Set DATEFORMAT so that the date strings are interpreted correctly regardless of
+   the default DATEFORMAT on the server.
+*/
+SET DATEFORMAT mdy
+GO
+use "Northwind"
+go
+if exists (select * from sysobjects where id = object_id('dbo.Employee Sales by Country') and sysstat & 0xf = 4)
+	drop procedure "dbo"."Employee Sales by Country"
+GO
+if exists (select * from sysobjects where id = object_id('dbo.Sales by Year') and sysstat & 0xf = 4)
+	drop procedure "dbo"."Sales by Year"
